62a3be529c166357de212dcb830aa4da0e608020
Marcos Cardoso <marcosjtc1973@gmail.com>
2022-06-14 21:06:57 -0300

    querySelector na console

---
+++ main.js
@@ -0,0 +1,4 @@
+//Convenção de nome javascript para utilizar este nome como o arquivo principal.
+//alert('olá mundo!');
+//Seleciona uma tag que contenha o elemento na pequisa (Pode usar no console,).
+document.querySelector('.tecla_pom');
