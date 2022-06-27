2d08c5623dd0e035fad676084aead83aad2ce9cf
Marcos Cardoso <marcosjtc1973@gmail.com>
2022-06-27 18:08:56 -0300

    Adicionando laço while

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
+function tocaSomPom(){
+  document.querySelector('#som_tecla_pom').play();  
+}
+
 //Convenção de nome javascript para utilizar este nome como o arquivo principal.
 //alert('olá mundo!');
 //Seleciona uma tag que contenha o elemento na pequisa (Pode usar no console,).
-document.querySelector('.tecla_pom');
+//Retirada do parêntesis é necessária para guardar o nome da function,
+//no evento onclick.
+//document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//Seleciona todos os botões.
+//const listaDeTeclas = document.querySelectorAll('input[type=button]');
+//console.log(listaDeTeclas);
+
